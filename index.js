9935bb390fa3b9ad94bc4f5483735e80c381afc6
Alfons-ES <alfonsersv@gmail.com>
2025-04-24 11:56:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let resetEl = document.getElementById("reset-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+let countMain = 0
+
+function addon() {
+    countMain += count 
+    countEl.textContent = countMain
+}
+
+function increment() {
+    count += 1
+    document.getElementById("addon-btn").textContent = "Add " + count
+}
+
+function reduce() {
+    count -= 1
+    document.getElementById("addon-btn").textContent = "Add " + count
+}
+
+function reset() {
+    let countStr = countEl.textContent + ".  "
+    resetEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+    countMain = 0
+    document.getElementById("addon-btn").textContent = "Add"
+}
